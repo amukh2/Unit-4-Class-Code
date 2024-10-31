87f58a87f83b12c69475e67c4fcc1017a219978e
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-10-31 14:47:46 -0700

    update

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,4 +1,7 @@
 package lesson1;
+import java.util.Scanner;
+
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +10,20 @@ public class Main {
     }
 
     public static void guessMyNumber(){
+        int secretNumber = (int) Math.round(Math.random() * 10);
+        int num = -1;
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("See if you can guess the number!");
+        System.out.println("Enter your guess: ");
+        num = sc.nextInt();
+        while (num != secretNumber){
+            System.out.println("Try Again!");
+            System.out.print("Enter your guess: ");
+            num = sc.nextInt();
+        }
+        System.out.println("Correct!");
 
     }
 }
