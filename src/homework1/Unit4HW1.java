7c02f80abe5446d6d68a8586a74b9bd9e52832e8
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-11-18 14:34:48 -0800

    update

--- src/homework1/Unit4HW1.java
+++ src/homework1/Unit4HW1.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Unit4HW1 {
 
     public static void main(String[] args) {
-        //guessingGame();
-        //tiredTurtle();
+        guessingGame();
+        tiredTurtle();
         notATamagotchi();
 
     }
@@ -60,17 +60,42 @@ public class Unit4HW1 {
 
 
     /**
-     * Write your own description
+     * Take care of a pet and make sure it does not die of starvation within
+     * 5 hours. If you pass it will be content at the end of the simulation.
      */
-    public static void notATamagotchi(){
+    public static void notATamagotchi() {
         Scanner sc = new Scanner(System.in);
         int hour = 1;
-        int hungerLevel = 10;
-        while (hour <= 5) {
-            System.out.printf("Hour: %d Current hunger level is %d Do you want to feed your pet? (yes/no): ", hour, hungerLevel);
-            String feed = sc.nextLine();
-            hour++;
+        int hungerLevel = 0;
+        boolean petDead = false;
 
+        while (hour < 5) {
+            hungerLevel += 10;
+            if (hungerLevel < 40) {
+                System.out.printf("\nHour: %d Current hunger level is %d Do you want to feed your pet? (yes/no): ", hour, hungerLevel);
+            } else {
+                System.out.printf("\nHour: %d Current hunger level is %d Feed your pet, it is unhappy. " +
+                        "\nDo you want to feed your pet? (yes/no): ", hour, hungerLevel);
+            }
+            String feed = sc.nextLine();
+            if (feed.equalsIgnoreCase("yes")) {
+                if (hungerLevel <= 25) {
+                    hungerLevel = 0;
+                } else {
+                    hungerLevel -= 25;
+                }
+            } else if (feed.equalsIgnoreCase("no") && hungerLevel >= 40) {
+                System.out.print("End of Simulation. Your pet is dead.");
+                petDead = true;
+                break;
+            }
+            hour++;
+        }
+        hungerLevel += 10;
+        if (hungerLevel >= 40 && !petDead) {
+            System.out.printf("\nHour 5: Current hunger level is %d End of Simulation. Your pet is dead.", hungerLevel);
+        } else if (!petDead) {
+            System.out.printf("\nHour 5: Current hunger level is %d End of Simulation. Your pet is content.", hungerLevel);
         }
     }
 
