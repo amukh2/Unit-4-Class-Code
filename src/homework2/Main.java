5f80b37376cd3d033ccae765252e4e6c0c72cddb
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-11-18 15:04:26 -0800

    update

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -9,26 +9,57 @@ public class Main {
         System.out.println();
         vowelCounter("aaeeeeiiiioouuuu");
         System.out.println();
-        palindromeChecker("level");
-        System.out.println();
-        palindromeChecker("abcde");
-        System.out.println();
-        palindromeChecker("racecar");
-        System.out.println();
-        palindromeChecker("aBbA");
-        System.out.println();
-        //extra credit
-        palindromeChecker("s*dfj*js");
-        System.out.println();
-        palindromeChecker("jf&fj");
+//        palindromeChecker("level");
+//        System.out.println();
+//        palindromeChecker("abcde");
+//        System.out.println();
+//        palindromeChecker("racecar");
+//        System.out.println();
+//        palindromeChecker("aBbA");
+//        System.out.println();
+//        //extra credit
+//        palindromeChecker("s*dfj*js");
+//        System.out.println();
+//        palindromeChecker("jf&fj");
 
     }
 
     public static void vowelCounter(String str) {
-
+        int countA, countE, countI, countO, countU;
+        countA = countE = countI = countO = countU = 0;
+        char current;
+        for (int i = 0; i < str.length(); i++) {
+            current = str.toLowerCase().charAt(i);
+            switch (current){
+                case 'a': countA++;
+                    break;
+                case 'e': countE++;
+                    break;
+                case 'i': countI++;
+                    break;
+                case 'o': countO++;
+                    break;
+                case 'u': countU++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.printf("""
+                Number of a's: %d
+                Number of e's: %d
+                Number of i's: %d
+                Number of o's: %d 
+                Number of u's: %d
+                
+                """, countA, countE, countI, countO, countU);
     }
 
     public static void palindromeChecker(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i)) && Character.isLetter(str.charAt(i))) {
 
+            }
+        }
     }
 }
