4e52e65d751bcba9ec5aa41d4291bc658c69d3d4
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-12-03 15:02:17 -0800

    update

--- src/homework3/Unit4HW3.java
+++ src/homework3/Unit4HW3.java
@@ -1,15 +1,11 @@
 package homework3;
-
 import java.util.Scanner;
-
 public class Unit4HW3 {
     public static void main(String []args){
         String secretWord = "ARBOR";
         System.out.println("------- WOODLE-------- ");
         Woodle(secretWord);
-
     }
-
     public static void Woodle(String answer){
         Scanner sc = new Scanner(System.in);
         System.out.println("Guess a 5 letter word:");
@@ -20,10 +16,12 @@ public class Unit4HW3 {
        while (!(input.toUpperCase().equals(answer))) {
            String tempAnswer = answer;
            for (int i = 0; i < 5; i++) {
+               int letter = tempAnswer.indexOf(input.substring(i,i+1).toUpperCase());
                if (input.substring(i,i+1).toUpperCase().equals(answer.substring(i,i+1))) {
                    green +=1;
-                   tempAnswer = answer.substring(0,i) + (answer.substring(i+1,5));
-               } else if (tempAnswer.indexOf(input.substring(i,i+1).toUpperCase()) != -1) {
+                   tempAnswer = answer.substring(0,i) + "*" + (answer.substring(i+1,5));
+               } else if (letter != -1 && !(input.substring(i,i+1).equals(input.substring(letter,letter+1)))) {
+                    tempAnswer = tempAnswer.substring(0,letter) + "*" + tempAnswer.substring(letter+1,5);
                     yellow +=1;
                }
            }
@@ -36,6 +34,6 @@ public class Unit4HW3 {
            input = sc.nextLine();
        }
        index+=1;
-        System.out.printf("Congratulations. It took you %s tries", index);
+        System.out.printf("Congratulations. It took you %d tries", index);
     }
 }
